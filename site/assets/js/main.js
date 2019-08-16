48c62de4976eb31e41681289e790896e113b691d
serii <seriiburduja@gmail.com>
2019-08-16 17:34:19 +0300

    links

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,22 +1,4 @@
 $(function () {
-	/*
-	https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
-	===========================*/
-	// $("html").easeScroll({
-	// 	frameRate: 60,
-	// 	animationTime: 1000,
-	// 	stepSize: 90,
-	// 	pulseAlgorithm: 1,
-	// 	pulseScale: 8,
-	// 	pulseNormalize: 1,
-	// 	accelerationDelta: 20,
-	// 	accelerationMax: 1,
-	// 	keyboardSupport: true,
-	// 	arrowScroll: 50,
-	// 	touchpadSupport: true,
-	// 	fixedBackground: true
-	// });
-
 	let sandwitch = function () {
 		let mainMenu = $('#js-main-menu');
 
@@ -100,7 +82,7 @@ $(function () {
 			$('#js-main-menu li a').on('click', function (e) {
 				let href = $(this).attr('href');
 
-				if (href === 'http://wp-prodinfo.loc/#js-offers') {
+				if (href === 'http://prodinfo.myihor.ru/#js-offers') {
 					e.preventDefault();
 					$('html, body').animate({
 						scrollTop: aboutOffsetTop
